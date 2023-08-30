fe4ce4cca3434d951645ca78aa0ed7fad0c4f904
pedro2398 <pedrooo2398@gmail.com>
2023-08-30 20:37:51 -0300

    Conclusão CP1

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,13 +1,72 @@
 package br.com.fiap;
 
+import br.com.fiap.domain.entity.Bem;
+import br.com.fiap.domain.entity.Departamento;
+import br.com.fiap.domain.entity.Inventario;
+import br.com.fiap.domain.entity.TipoDeBem;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import java.util.List;
+import javax.swing.*;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory( "oracle" );
+        EntityManager manager = factory.createEntityManager();
 
+        //saveData( manager );
+        findById( manager );
+        //findAll( manager );
 
+        manager.close();
+        factory.close();
     }
+
+    private static void saveData( EntityManager manager ) {
+
+        Departamento departamento = new Departamento()
+                .setNome( "Logistica1" );
+
+        TipoDeBem tipoBem = new TipoDeBem()
+                .setNome( "material1" );
+
+        Inventario inventario = new Inventario()
+                .setDepartamento( departamento )
+                .setFim( null )
+                .setInicio( LocalDate.now() )
+                .setRelatorio( "Teste1" );
+
+        Bem bem = new Bem()
+                .setNome("Carro1")
+                .setEtiqueta( "veiculo1" )
+                .setLocalizacao( departamento )
+                .setAquisicao( LocalDate.now() )
+                .setTipo( tipoBem);
+
+        manager.getTransaction().begin();
+        manager.persist( bem );
+        manager.persist( inventario );
+        manager.persist( departamento );
+        manager.persist( tipoBem );
+
+        manager.getTransaction().commit();
+    }
+
+    private static void findAll(EntityManager manager) {
+        List<Bem> list = manager.createQuery( "FROM Bem" ).getResultList();
+
+        list.forEach( System.out::println );
+    }
+
+    private static void findById(EntityManager manager) {
+        Long id = Long.valueOf( JOptionPane.showInputDialog( "ID do Processo" ) );
+        Inventario inventario = manager.find( Inventario.class, id );
+        System.out.println( inventario );
+    }
+
+
 }
--- src/main/java/br/com/fiap/domain/entity/Bem.java
+++ src/main/java/br/com/fiap/domain/entity/Bem.java
@@ -2,19 +2,38 @@ package br.com.fiap.domain.entity;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "TB_BEM", uniqueConstraints = {
+        @UniqueConstraint( name = "UK_BEM_ETIQUETA_BEM", columnNames = "ETIQUETA_BEM")
+})
 public class Bem {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_BEM")
+    @SequenceGenerator(name = "SQ_BEM", sequenceName = "SQ_BEM")
+    @Column(name = "ID_BEM")
     private Long id;
 
+    @Column(name = "NM_BEM", nullable = false)
     private String nome;
-
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(
+            name = "ID_TIPO_BEM",
+            referencedColumnName = "ID_TIPO_BEM",
+            foreignKey = @ForeignKey(name = "FK_BEM_TIPO_BEM")
+    )
     private TipoDeBem tipo;
-
+    @Column(name = "ETIQUETA_BEM", nullable = false)
     private String etiqueta;
-
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(
+            name = "ID_DEPARTAMENTO",
+            referencedColumnName = "ID_DEPARTAMENTO",
+            foreignKey = @ForeignKey(name = "FK_BEM_DEPARTAMENTO")
+    )
     private Departamento localizacao;
-
+    @Column(name = "DT_AQUISICAO", nullable = false)
     private LocalDate aquisicao;
 
 
